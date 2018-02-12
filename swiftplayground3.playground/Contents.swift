b9433af609c2560d9d2160e65b3b3004afd06968
jaquawn <jd23@hawaii.edu>
2018-02-12 10:25:20 -1000

    Switftplayground

---
+++ swiftplayground3.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var fruitBasket : [String]
+
+var simpleString: String
+
+fruitBasket = [ "kiwi", "Apple", "Watermelon", "Pears"]
+
+print(fruitBasket)
+
+
+for name in fruitBasket {
+    print (" I like to eat " + name )
+}
+
+
+print (fruitBasket[1])
+print (fruitBasket[0])
+print (fruitBasket[2])
+print (fruitBasket [3])
+
